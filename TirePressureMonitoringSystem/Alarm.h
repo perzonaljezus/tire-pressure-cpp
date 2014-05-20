e85773ce0752266192d2aff231461ab9ba8dcd29
Ben Wu <wubin28@gmail.com>
2014-05-20 21:57:07 +0800

    Finished Alarm.h.

--- TirePressureMonitoringSystem/Alarm.h
+++ TirePressureMonitoringSystem/Alarm.h
@@ -2,6 +2,14 @@
 #define ALARM_H
 
 class Alarm {
+public:
+	void check();
+	bool isAlarmOn();
+private:
+	const double LowPressureThreshold = 17;
+	const double HighPressureThreshold = 21;
+	Sensor sensor = new Sensor;
+	bool alarmOn = false;
 };
 
 #endif /* ALARM_H */
