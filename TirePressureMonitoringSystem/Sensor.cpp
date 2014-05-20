c6c5d96f355d57c2e831a627727eeafede895230
Ben Wu <wubin28@gmail.com>
2014-05-20 22:56:21 +0800

    Added and finished file Sensor.cpp and updated file Sensor.h.

---
+++ TirePressureMonitoringSystem/Sensor.cpp
@@ -0,0 +1,16 @@
+#include "Sensor.h"
+
+double Sensor::popNextPressurePsiValue() {
+	double pressureTelemetryValue;
+	pressureTelemetryValue = samplePressure();
+
+	return OFFSET + pressureTelemetryValue;
+}
+
+double Sensor::samplePressure() {
+	// placeholder implementation that simulate a real sensor in a real tire
+	std::default_random_engine engine;
+	std::uniform_real_distribution<double> uniform(0,1);
+	double pressureTelemetryValue = 6 * uniform(engine) * uniform(engine);
+	return pressureTelemetryValue;
+}
--- TirePressureMonitoringSystem/Sensor.h
+++ TirePressureMonitoringSystem/Sensor.h
@@ -1,6 +1,8 @@
 #ifndef SENSOR_H
 #define SENSOR_H
 
+#include <random>
+
 class Sensor {
 public:
 	static const double OFFSET = 16;
