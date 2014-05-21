fbe015a439c981183754c9227d2abf37d1aa8855
Ben Wu <wubin28@gmail.com>
2014-05-21 23:05:12 +0800

    Wrote intention code in class StubSensor to use a queue to store the pressure values.

--- Exercise.ProposedSolution/TirePressureMonitoringSystem/StubSensor.cpp
+++ Exercise.ProposedSolution/TirePressureMonitoringSystem/StubSensor.cpp
@@ -1,12 +1,21 @@
 #include "StubSensor.h"
 
 void StubSensor::setStubPressureValue(double pressureValue) {
-	stubPressureValue = pressureValue;
+	queue.push(pressureValue);
 }
 
 void StubSensor::setStubPressureValue(double pressureValue1, double pressureValue2, double pressureValue3) {
+	queue.push(pressureValue1);
+	queue.push(pressureValue2);
+	queue.push(pressureValue3);
 }
 
 double StubSensor::popNextPressurePsiValue() {
-	return stubPressureValue;
+	if (queue.empty()) {
+		return 0.0;
+	}
+
+	double pressureValue = queue.front();
+	queue.pop();
+	return pressureValue;
 }
